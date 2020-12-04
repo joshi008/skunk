fb82698df5e9d11e2cd51c6fe0d5787a524295dc
Hrishabh Joshi <joshi008@users.noreply.github.com>
2020-12-04 23:24:06 +0530

    changes

---
+++ vendor/assets/javascript/fastruby/styleguide.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require popper
+//= require bootstrap
+//= require material/mdlComponentHandler
+//= require material/rAF
+//= require material/textfield
+//= require material/slider
+//= require ./custom
